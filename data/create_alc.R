1458c34e0c418a20637c6a71207ad92822281cce
Sina Hulkkonen <sina.hulkkonen@helsinki.fi>
2018-11-13 19:07:14 +0200

    Update data/create_alc.R

--- data/create_alc.R
+++ data/create_alc.R
@@ -5,4 +5,18 @@
 library(foreign)
 library(dplyr)
 
-math_url<-read.csv("")
+#reading the file math & exploring its structure and dimensions
+url1<-("/Users/sina/Library/Mobile Documents/com~apple~CloudDocs/tutkimus/tohtorikoulutus/GitHub/IODS-project/data")
+url_math<-paste(url1,"student-mat.csv", sep = "/")
+math <- read.table(url_math, sep = ";" , header=TRUE)
+colnames(math)
+str(math)
+dim(math)
+
+#reading the file por & exploring its structure and dimensions
+url2<-("/Users/sina/Library/Mobile Documents/com~apple~CloudDocs/tutkimus/tohtorikoulutus/GitHub/IODS-project/data")
+url_por<-paste(url1,"student-por.csv", sep = "/")
+por <- read.table(url_por, sep = ";" , header=TRUE)
+colnames(por)
+str(por)
+dim(por)
