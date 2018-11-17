6a10dace6aef43354e03ce537074af8d4e592882
Sina Hulkkonen <sina.hulkkonen@helsinki.fi>
2018-11-17 14:40:16 +0200

    updates

--- data/create_alc.R
+++ data/create_alc.R
@@ -2,6 +2,7 @@
 #Sina Hulkkonen
 #13/11/2018
 
+install.packages("dplyr")
 library(foreign)
 library(dplyr)
 
@@ -20,3 +21,47 @@ por <- read.table(url_por, sep = ";" , header=TRUE)
 colnames(por)
 str(por)
 dim(por)
+
+#joinin the two datasets
+join_by <- c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet")
+math_por <- inner_join(math, por, by = join_by,suffix=c(".math", ".por"))
+
+#exloring the structure and dimensions of the joined dataset
+str(math_por)
+dim(math_por)
+
+# create a new data frame with only the joined columns
+alc <- select(math_por, one_of(join_by))
+
+# columns that were not used for joining the data
+notjoined_columns <- colnames(math)[!colnames(math) %in% join_by]
+
+# print out the columns not used for joining
+notjoined_columns
+
+# for every column name not used for joining...
+for(column_name in notjoined_columns) {
+  # select two columns from 'math_por' with the same original name
+  two_columns <- select(math_por, starts_with(column_name))
+  # select the first column vector of those two columns
+  first_column <- select(two_columns, 1)[[1]]
+  
+  # if that first column  vector is numeric...
+  if(is.numeric(first_column)) {
+    # take a rounded average of each row of the two columns and
+    # add the resulting vector to the alc data frame
+    alc[column_name] <- round(rowMeans(two_columns))
+  } else { # else if it's not numeric...
+    # add the first column vector to the alc data frame
+    alc[column_name] <- first_column
+  }
+}
+
+#taking a look to the new dataset alc
+glimpse(alc)
+
+
+#alcohol consumption
+alc <- mutate(alc, alc_use = (Dalc + Walc) / 2)
+# define a new logical column 'high_use'
+alc <- mutate(alc, high_use = alc_use > 2)
