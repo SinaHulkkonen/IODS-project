75bfff3f4da2fa87e8e7ee9ea19942c5752e467b
Sina Hulkkonen <sina.hulkkonen@helsinki.fi>
2018-12-01 22:21:35 +0200

    week5 updates

--- data/create_human.R
+++ data/create_human.R
@@ -29,3 +29,41 @@ str(human)
 
 #make a file
 write.table(human, file = "create_human.R")
+
+##WEEK 5 EXERCISES
+
+#1. please look above for the variables I made (plus their names in my dataset)
+str(human)
+summary(human)
+dim(human)
+#human is a dataset combined of two datasets: hd and gii. it describes populations: life expectancy, labour rate, education. it has 195 observations in 19 variables.
+
+#studying the structure of human variable GNI and mutating it into numeric
+str(human$GNI)
+str_replace(human$GNI, pattern=",", replace ="")%>%as.numeric
+
+#2. selecting the columns to keep (notice that the column names are made by me!)
+keep <- c("country", "edu_ratio", "labour_ratio", "life_exp", "edu_exp", "GNI", "mat_mortality", "adol_birth", "parliament")
+
+human <- select(human, one_of(keep))
+
+#3. selecting only complete cases
+human_ <- filter(human, complete.cases(human))
+
+#4. finding out wchich rows refer to regions instead of countries
+human_$country
+
+#the last seven rows (156-162) are not countries and are excluded
+last <- nrow(human_) - 7
+human_ <- human_[1:last, ]
+
+#5. add countries as rownames
+rownames(human_) <- human_$country
+# remove the country variable
+human_ <- select(human_, -country)
+
+#overwriting the formen dataset "human"
+human<-human_
+#make a file
+write.table(human, file = "create_human.R")
+human
